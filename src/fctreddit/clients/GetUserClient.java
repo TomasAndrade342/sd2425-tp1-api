f7c442cc782eb550bf8bd163d0b2d68ace54798c
Paulo Aires <paulo.aires95@gmail.com>
2025-04-08 13:41:39 +0100

    Added Delete, Update and Modified Get UserClient.

--- src/fctreddit/clients/GetUserClient.java
+++ src/fctreddit/clients/GetUserClient.java
@@ -1,9 +1,11 @@
 package fctreddit.clients;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.URI;
 import java.util.logging.Logger;
 
+import fctreddit.api.Discovery;
 import fctreddit.api.User;
 import fctreddit.api.java.Result;
 import fctreddit.clients.grpc.GrpcUsersClient;
@@ -17,21 +19,26 @@ public class GetUserClient {
 
     public static void main(String[] args) throws IOException {
 
-        if( args.length != 3) {
-            System.err.println( "Use: java " + CreateUserClient.class.getCanonicalName() + " url userId password");
+        if( args.length != 4) {
+            System.err.println( "Use: java " + GetUserClient.class.getCanonicalName() + " mcIP mcPort userId password");
             return;
         }
+        String multiCastIp = args[0];
+        int multiCastPort = Integer.valueOf(args[1]);
+        String userId = args[2];
+        String password = args[3];
 
-        String serverUrl = args[0];
-        String userId = args[1];
-        String password = args[2];
+        Discovery disc = new Discovery(new InetSocketAddress(multiCastIp, multiCastPort));
+        disc.start();
+        URI[] knownUris = disc.knownUrisOf("Users", 1);
+        URI serverUrl = knownUris[0];
 
         UsersClient client = null;
 
-        if(serverUrl.endsWith("rest"))
-            client = new RestUsersClient( URI.create( serverUrl ) );
+        if(serverUrl.getPath().endsWith("rest"))
+            client = new RestUsersClient(serverUrl);
         else
-            client = new GrpcUsersClient( URI.create( serverUrl) );
+            client = new GrpcUsersClient( serverUrl);
 
         Result<User> result = client.getUser(userId, password);
         if( result.isOK()  )
