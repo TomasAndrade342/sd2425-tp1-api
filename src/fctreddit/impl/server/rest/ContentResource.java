a10f7adc05971cc4875b2a8da5dc71fc4a619f2d
TomasAndrade342 <andradetomas715@gmail.com>
2025-04-18 19:14:38 +0100

    Implemented ContentResource

--- src/fctreddit/impl/server/rest/ContentResource.java
+++ src/fctreddit/impl/server/rest/ContentResource.java
@@ -2,11 +2,15 @@ package fctreddit.impl.server.rest;
 
 import fctreddit.api.Post;
 import fctreddit.api.java.Content;
+import fctreddit.api.java.Result;
 import fctreddit.api.rest.RestContent;
 import fctreddit.impl.server.java.JavaContent;
+import jakarta.ws.rs.WebApplicationException;
 
 import java.util.List;
 import java.util.logging.Logger;
+
+import static fctreddit.impl.server.rest.UsersResource.errorCodeToStatus;
 
 public class ContentResource implements RestContent {
 
@@ -20,61 +24,128 @@ public class ContentResource implements RestContent {
 
     @Override
     public String createPost(Post post, String userPassword) {
-        return "";
+        Log.info("createPost: post = " + post + "; userPassword = " + userPassword);
+
+        Result<String> res = impl.createPost(post, userPassword);
+        if(!res.isOK()) {
+            throw new WebApplicationException(errorCodeToStatus(res.error()));
+        }
+        return res.value();
     }
 
     @Override
     public List<String> getPosts(long timestamp, String sortOrder) {
-        return List.of();
+        Log.info("getPosts: timestamp = " + timestamp + "; sortOrder = " + sortOrder);
+
+        Result<List<String>> res = impl.getPosts(timestamp, sortOrder);
+        if(!res.isOK()) {
+            throw new WebApplicationException(errorCodeToStatus(res.error()));
+        }
+        return res.value();
     }
 
     @Override
     public Post getPost(String postId) {
-        return null;
+        Log.info("getPost: postId = " + postId);
+
+        Result<Post> res = impl.getPost(postId);
+        if(!res.isOK()) {
+            throw new WebApplicationException(errorCodeToStatus(res.error()));
+        }
+        return res.value();
     }
 
     @Override
     public List<String> getPostAnswers(String postId, long timeout) {
-        return List.of();
+        Log.info("getPostAnswers: postId =" + postId + "; timeout = " + timeout);
+
+        Result<List<String>> res = impl.getPostAnswers(postId, timeout);
+        if(!res.isOK()) {
+            throw new WebApplicationException(errorCodeToStatus(res.error()));
+        }
+        return res.value();
     }
 
     @Override
     public Post updatePost(String postId, String userPassword, Post post) {
-        return null;
+        Log.info("updatePost: postId = " + postId + "; userPassword = " + userPassword + "; post = " + post);
+
+        Result<Post> res = impl.updatePost(postId, userPassword, post);
+        if(!res.isOK()) {
+            throw new WebApplicationException(errorCodeToStatus(res.error()));
+        }
+        return res.value();
     }
 
     @Override
     public void deletePost(String postId, String userPassword) {
+        Log.info("deletePost: postId = " + postId + "; userPassword = " + userPassword);
 
+        Result<Void> res = impl.deletePost(postId, userPassword);
+        if(!res.isOK()) {
+            throw new WebApplicationException(errorCodeToStatus(res.error()));
+        }
     }
 
     @Override
     public void upVotePost(String postId, String userId, String userPassword) {
+        Log.info("upVotePost: postId = " + postId + "; userId = " + userId + "; userPassword = " + userPassword);
 
+        Result<Void> res = impl.upVotePost(postId, userId, userPassword);
+        if(!res.isOK()) {
+            throw new WebApplicationException(errorCodeToStatus(res.error()));
+        }
     }
 
     @Override
     public void removeUpVotePost(String postId, String userId, String userPassword) {
+        Log.info("removeUpVotePost: postId = " + postId + "; userId = " + userId + "; userPassword = " + userPassword);
 
+        Result<Void> res = impl.removeUpVotePost(postId, userId, userPassword);
+        if(!res.isOK()) {
+            throw new WebApplicationException(errorCodeToStatus(res.error()));
+        }
     }
 
     @Override
     public void downVotePost(String postId, String userId, String userPassword) {
+        Log.info("downVotePost: postId = " + postId + "; userId = " + userId + "; userPassword = " + userPassword);
 
+        Result<Void> res = impl.downVotePost(postId, userId, userPassword);
+        if(!res.isOK()) {
+            throw new WebApplicationException(errorCodeToStatus(res.error()));
+        }
     }
 
     @Override
     public void removeDownVotePost(String postId, String userId, String userPassword) {
+        Log.info("downVotePost: postId = " + postId + "; userId = " + userId + "; userPassword = " + userPassword);
 
+        Result<Void> res = impl.downVotePost(postId, userId, userPassword);
+        if(!res.isOK()) {
+            throw new WebApplicationException(errorCodeToStatus(res.error()));
+        }
     }
 
     @Override
     public Integer getupVotes(String postId) {
-        return 0;
+        Log.info("getUpVotes: postId = " + postId);
+
+        Result<Integer> res = impl.getupVotes(postId);
+        if(!res.isOK()) {
+            throw new WebApplicationException(errorCodeToStatus(res.error()));
+        }
+        return res.value();
     }
 
     @Override
     public Integer getDownVotes(String postId) {
-        return 0;
+        Log.info("getDownVotes: postId = " + postId);
+
+        Result<Integer> res = impl.getDownVotes(postId);
+        if(!res.isOK()) {
+            throw new WebApplicationException(errorCodeToStatus(res.error()));
+        }
+        return res.value();
     }
 }
