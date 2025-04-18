5ed934a9ff95107ee6cd7058bc3359597d04c45c
Paulo Aires <paulo.aires95@gmail.com>
2025-04-18 19:15:57 +0100

    GrpcContentServerStub.java added methods.

--- src/fctreddit/impl/server/grpc/GrpcContentServerStub.java
+++ src/fctreddit/impl/server/grpc/GrpcContentServerStub.java
@@ -2,9 +2,12 @@ package fctreddit.impl.server.grpc;
 
 import fctreddit.api.java.Content;
 import fctreddit.impl.grpc.generated_java.ContentGrpc;
+import fctreddit.impl.grpc.generated_java.ContentProtoBuf;
+import fctreddit.impl.grpc.generated_java.ImageProtoBuf;
 import fctreddit.impl.server.java.JavaContent;
 import io.grpc.BindableService;
 import io.grpc.ServerServiceDefinition;
+import io.grpc.stub.StreamObserver;
 
 public class GrpcContentServerStub implements ContentGrpc.AsyncService, BindableService {
 
@@ -15,4 +18,55 @@ public class GrpcContentServerStub implements ContentGrpc.AsyncService, Bindable
         return ContentGrpc.bindService(this);
     }
 
+    public void createPost(ContentProtoBuf.CreatePostArgs request, StreamObserver<ContentProtoBuf.CreatePostResult> responseObserver) {
+
+    }
+
+    public void getPosts(ContentProtoBuf.GetPostsArgs request, StreamObserver<ContentProtoBuf.GetPostsResult> responseObserver) {
+
+    }
+
+    public void getPost (ContentProtoBuf.GetPostArgs request, StreamObserver<ContentProtoBuf.GrpcPost> responseObserver) {
+
+    }
+
+    public void getPostAnswers (ContentProtoBuf.GetPostAnswersArgs request, StreamObserver<ContentProtoBuf.GetPostsResult> responseObserver) {
+
+    }
+
+    public void updatePost (ContentProtoBuf.UpdatePostArgs request, StreamObserver<ContentProtoBuf.GrpcPost> responseObserver) {
+
+    }
+
+    public void deletePost (ContentProtoBuf.DeletePostArgs request, StreamObserver<ContentProtoBuf.EmptyMessage> responseObserver) {
+
+    }
+
+    public void upVotePost (ContentProtoBuf.ChangeVoteArgs request, StreamObserver<ContentProtoBuf.EmptyMessage> responseObserver) {
+
+    }
+
+    public void removeUpVotePost (ContentProtoBuf.ChangeVoteArgs request, StreamObserver<ContentProtoBuf.EmptyMessage> responseObserver) {
+
+    }
+
+    public void downVotePost (ContentProtoBuf.ChangeVoteArgs request, StreamObserver<ContentProtoBuf.EmptyMessage> responseObserver) {
+
+    }
+
+    public void removeDownVotePost (ContentProtoBuf.ChangeVoteArgs request, StreamObserver<ContentProtoBuf.EmptyMessage> responseObserver) {
+
+    }
+
+    public void getUpVotes (ContentProtoBuf.GetPostArgs request, StreamObserver<ContentProtoBuf.VoteCountResult> responseObserver) {
+
+    }
+
+    public void getDownVotes (ContentProtoBuf.GetPostArgs request, StreamObserver<ContentProtoBuf.VoteCountResult> responseObserver) {
+
+    }
 }
+
+
+
+
